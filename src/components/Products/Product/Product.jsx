1bf29c06d4603a17f00f0037f7f2dac5d474112a
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-09-02 18:48:38 -0100

    commit

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCartOutlined } from '@material-ui/icons'
-import { classes } from 'istanbul-lib-coverage'
+import useStyles from './styles'
 
 const Product = ({ product }) => {
+    const classes = useStyles();
+
     return (
         <Card className={classes.root}>
             <CardMedia 
