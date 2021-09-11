332aa852363599467249f353df7a02d14c364d24
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-09-10 23:29:02 -0100

    commit

--- src/components/Products/Banner/Banner.jsx
+++ src/components/Products/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import bg from '../../../assets/bg.jpg'
+import bg from '../../../assets/bg2.jpg'
 import { BsArrowRightShort } from 'react-icons/bs'
 import './banner.css'
 
